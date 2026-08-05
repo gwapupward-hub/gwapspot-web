2f938760f2090c1d130985cb69bd0fdc01089895
New Perspective <gwapupward@gmail.com>
2026-08-05 06:47:19 -0400

    fix: remove conflicting www redirect

--- next.config.ts
+++ next.config.ts
@@ -17,16 +17,6 @@ const nextConfig: NextConfig = {
   async headers() {
     return [{ source: "/(.*)", headers: securityHeaders }];
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.gwapspot.com" }],
-        destination: "https://gwapspot.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
