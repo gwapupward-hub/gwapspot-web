75463fed8fa7441003eb83f7effc15d7ec9fb8b6
New Perspective <gwapupward@gmail.com>
2026-08-05 06:48:00 -0400

    fix: align robots host with primary domain

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/"],
     },
-    sitemap: "https://gwapspot.com/sitemap.xml",
-    host: "https://gwapspot.com",
+    sitemap: "https://www.gwapspot.com/sitemap.xml",
+    host: "https://www.gwapspot.com",
   };
 }
